5aeaa2fc3ec5b442896ed8cfdfe2dd6136580aec
lilybrevec <allabrevec@gmail.com>
2020-05-16 15:56:20 +0900

    change fonts

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ extern crate tokio;
 use iced::{Settings, Application};
 #[tokio::main]
 async fn main() -> Result<(), failure::Error> {
-  gelato::App::run(Settings::default());
+  let mut sets = Settings::default();
+  sets.default_font = Some(include_bytes!("../fonts/M_PLUS_Rounded_1c/MPLUSRounded1c-Medium.ttf"));
+  gelato::App::run(sets);
   Ok(())
 }
